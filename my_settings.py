9d77b1ec8371649492c9dae32e9873c8947b1e5c
sunghyunchoi <chltjdgus99@naver.com>
2020-12-30 19:21:00 +0900

    test_201230

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME' : 'django_test',
+        'USER' : 'root',
+        'PASSWORD' : '1234',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
+    }
+}
+
+SECRET_KEY = 'sm6blqp@nc9b(#wvpw3n-hbbtop8x0az62=vpyl23i+*l#yv8y'
